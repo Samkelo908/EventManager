163d1f4e0e779f2c4dd4f35032e614f7f698a413
Samkelo Maswana <129047768+Samkelo908@users.noreply.github.com>
2025-10-17 13:11:00 +0200

    Update

---
+++ EventManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class EventManager
+{
+    private HashSet<string> attendees;
+
+    public EventManager()
+    {
+        attendees = new HashSet<string>();
+    }
+
+    public void AddAttendee(string name)
+    {
+        if (attendees.Add(name))
+        {
+            Console.WriteLine($"\"{name}\" has been registered successfully.");
+        }
+        else
+        {
+            Console.WriteLine($"\"{name}\" is already registered.");
+        }
+    }
+
+    public void RemoveAttendee(string name)
+    {
+        if (attendees.Remove(name))
+        {
+            Console.WriteLine($"\"{name}\" has been removed.");
+        }
+        else
+        {
+            Console.WriteLine($"\"{name}\" was not found in the registration list.");
+        }
+    }
+
+    public void DisplayAllAttendees()
+    {
+        Console.WriteLine("--- Registered Attendees ---");
+
+        if (attendees.Count == 0)
+        {
+            Console.WriteLine("No one is registered yet.");
+        }
+        else
+        {
+            foreach (string attendee in attendees)
+            {
+                Console.WriteLine(attendee);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        EventManager manager = new EventManager();
+        bool running = true;
+
+        while (running)
+        {
+            Console.WriteLine("\nEvent Attendee Manager");
+            Console.WriteLine("1. Add Attendee");
+            Console.WriteLine("2. Remove Attendee");
+            Console.WriteLine("3. Display All Attendees");
+            Console.WriteLine("4. Exit");
+            Console.Write("Please enter your choice: ");
+
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    Console.Write("Enter attendee name: ");
+                    string nameToAdd = Console.ReadLine();
+                    if (!string.IsNullOrWhiteSpace(nameToAdd))
+                    {
+                        manager.AddAttendee(nameToAdd);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid name. Please try again.");
+                    }
+                    break;
+
+                case "2":
+                    Console.Write("Enter attendee name to remove: ");
+                    string nameToRemove = Console.ReadLine();
+                    if (!string.IsNullOrWhiteSpace(nameToRemove))
+                    {
+                        manager.RemoveAttendee(nameToRemove);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid name. Please try again.");
+                    }
+                    break;
+
+                case "3":
+                    manager.DisplayAllAttendees();
+                    break;
+
+                case "4":
+                    Console.WriteLine("Thank you for using Event Attendee Manager. Goodbye!");
+                    running = false;
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid choice. Please enter a number between 1 and 4.");
+                    break;
+            }
+        }
+    }
+}
